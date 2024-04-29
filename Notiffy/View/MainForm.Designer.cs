339b2ddae687112cb8d57b78e04b22eabda07d53
Jakub Adamczyk <jwf.adamczyk@gmail.com>
2024-04-29 08:59:54 +0200

    Icon

--- Notiffy/View/MainForm.Designer.cs
+++ Notiffy/View/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             SuspendLayout();
             // 
             // MainForm
@@ -35,6 +36,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MainForm";
             Text = "Notiffy 0.1";
             ResumeLayout(false);
